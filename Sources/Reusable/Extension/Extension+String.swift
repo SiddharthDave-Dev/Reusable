a251a78dbe4eae467d7d0764b5073e9d89ed9a47
SiddharthDave-Dev <davesiddharth76@gmail.com>
2025-06-08 19:09:43 +0530

    add

---
+++ Sources/Reusable/Extension/Extension+String.swift
@@ -0,0 +1,17 @@
+//
+//  Extension+String.swift
+//  ChatterBox
+//
+//  Created by Siddharth Dave on 3/31/25.
+//
+
+import UIKit
+
+
+extension String {
+    func width(usingFont font: UIFont) -> CGFloat {
+        let attributes = [NSAttributedString.Key.font: font]
+        let size = (self as NSString).size(withAttributes: attributes)
+        return size.width
+    }
+}
